e6c91042f89d23d3d75ee8f9244a31fae31fb2a5
malarc01 <45378877+malarc01@users.noreply.github.com>
2019-12-21 08:03:53 -0500

    added console.log

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function colorBg() {
 	var selectedId = document.getElementById('selectid');
 	var bgColor = selectedId.options[selectedId.selectedIndex].value;
+	console.log(selectedId.options[selectedId.selectedIndex]);
 	document.body.style.backgroundColor = bgColor;
 }
